45c0c6e18903808772672ec52987e33949724422
Alex <kelheor23@gmail.com>
2025-10-29 14:46:15 +0300

    parse_value_with_format relise

--- include/parse.hpp
+++ include/parse.hpp
@@ -5,17 +5,93 @@
 #include <string_view>
 #include <utility>
 #include <vector>
+#include <charconv>
 
 #include "types.hpp"
 
 namespace stdx::details {
 
-// здесь ваш код
+template <class Int>
+std::expected<Int, scan_error> parse_int(std::string_view s, int base) {
+    if (s.empty())
+        return std::unexpected(scan_error{"empty input for integer"});
+
+    if constexpr (std::is_unsigned_v<Int>) {
+        if (!s.empty() && s.front() == '-')
+            return std::unexpected(scan_error{"negative value for unsigned type"});
+    }
+
+    Int value{};
+    auto* first = s.data();
+    auto* last  = s.data() + s.size();
+    auto res = std::from_chars(first, last, value, base);
+    if (res.ec != std::errc{} || res.ptr != last)
+        return std::unexpected(scan_error{"invalid integer format"});
+    return value;
+}
+
+template <class Float>
+std::expected<Float, scan_error> parse_float(std::string_view s) {
+    if (s.empty())
+        return std::unexpected(scan_error{"empty input for floating"});
+
+    Float value{};
+    auto* first = s.data();
+    auto* last  = s.data() + s.size();
+    auto res = std::from_chars(first, last, value, std::chars_format::general);
+    if (res.ec != std::errc{} || res.ptr != last)
+        return std::unexpected(scan_error{"invalid floating format"});
+    return value;
+}
+
+enum class conv {
+    int_, uint_, float_, string_
+};
+
+inline std::expected<conv, scan_error> parse_conv(std::string_view fmt) {
+    if (fmt == "d") return conv::int_;
+    if (fmt == "s") return conv::string_;
+    if (fmt == "u") return conv::uint_;
+    if (fmt == "f") return conv::float_;
+    return std::unexpected(scan_error{"unknown conversion specifier: " + std::string(fmt)});
+}
 
 // Функция для парсинга значения с учетом спецификатора формата
 template <typename T>
 std::expected<T, scan_error> parse_value_with_format(std::string_view input, std::string_view fmt) {
-    // здесь ваш код
+    auto conv_kind_temp = parse_conv(fmt);
+    if (!conv_kind_temp)
+        return std::unexpected(std::move(conv_kind_temp.error()));
+
+    auto conv_kind = *conv_kind_temp;
+
+    switch (conv_kind)
+    {
+    case conv::string_:
+        if (!(std::is_same_v<T, std::string> || std::is_same_v<T, std::string_view>))
+            return std::unexpected(scan_error{"specifier 's' incompatible with destination type"});
+        if constexpr (std::is_same_v<T, std::string_view>)
+            return input;
+        else
+            return std::string(input);
+    case conv::float_:
+        if constexpr (std::is_floating_point_v<T>)
+            return parse_float<T>(input);
+        else 
+            return std::unexpected(scan_error{"specifier 'f' incompatible with destination type"});        
+    case conv::uint_:
+        if constexpr (!std::is_unsigned_v<T>)
+            return std::unexpected(scan_error{"specifier 'u' incompatible with destination type"});
+        else
+            return parse_int<T>(input, 10);
+    case conv::int_:
+        if constexpr (!std::is_signed_v<T>)
+            return std::unexpected(scan_error{"specifier 'd' incompatible with destination type"});
+        else 
+            return parse_int<T>(input, 10);    
+    default:
+        return std::unexpected(scan_error{"destination type incompatible with conversion specifier"});
+    }    
 }
 
 // Функция для проверки корректности входных данных и выделения из обеих строк интересующих данных для парсинга
